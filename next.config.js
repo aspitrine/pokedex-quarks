47f054b143df2606a5a9bface3228edd4c62fc3f
Aspitrine <loquente62@gmail.com>
2023-04-24 14:33:04 +0200

    try to fix ?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 module.exports = nextConfig;
